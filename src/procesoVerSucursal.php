9ba2506c470d6bd2850d8ca1584243d3070cafb5
Raul <nalvartediego40@gmail.com>
2022-10-11 10:07:56 -0500

    ordenando codigo agregar POO terminando eliminar y mostrar contactos

---
+++ src/procesoVerSucursal.php
@@ -0,0 +1,32 @@
+<?php include 'templates/header.php' ?>
+<?php include 'style/style.php' ?>
+
+<?php
+include_once 'connection/conexion.php';
+
+/**
+ * Datos  
+ * */
+$ruc = $_GET['ruc'];
+
+/**
+ * validando si no estan vacios 
+ * */
+validandoDatosVacios($ruc);
+
+/**
+ * Consulta
+ * */
+$consulta = $bd->query("SELECT * FROM  sucursal2 as s JOIN empresa2 as e WHERE s.ruc_empresa = $ruc and e.eliminada = 1;");
+$sucursal = $consulta->fetchAll(PDO::FETCH_OBJ);
+
+/**
+ * si los datos estan vacios no sigue con el proceso 
+ * */
+function validandoDatosVacios($ruc){
+    if( empty($ruc)){
+       $resultado = header('Location: pantallaVerContactos.php?mensaje=error');
+       return $resultado;
+    }
+}
+?>
